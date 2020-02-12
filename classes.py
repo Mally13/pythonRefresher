185a9818b2b2dd7c22589309900dea2ed15be54e
Mally13 <mallymutuku13@gmail.com>
2020-02-12 23:32:43 +0300

    new

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+class Dog:
+    def __init__(self,name="",age=0,furcolor=""):
+       self.name=name
+       self.age=age
+       self.furcolor=furcolor
+       dogInfo= "Hey, dogs are cool"
+       def bark(self):
+           print("BARK")
+
+mydog = Dog("Fido",13,"Brown")
+print(mydog.age)
